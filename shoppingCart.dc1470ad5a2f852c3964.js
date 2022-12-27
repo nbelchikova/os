5a95dfee47faec3a336820877ed4360e5a043387
nbelchikova <101473925+nbelchikova@users.noreply.github.com>
2022-12-27 11:20:49 +0300

    Add files via upload

---
+++ shoppingCart.dc1470ad5a2f852c3964.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";var e={580:(e,r,t)=>{t.r(r)}},r={};function t(o){var n=r[o];if(void 0!==n)return n.exports;var i=r[o]={exports:{}};return e[o](i,i.exports,t),i.exports}t.r=e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},t(580)})();
+//# sourceMappingURL=shoppingCart.dc1470ad5a2f852c3964.js.map
